7c6f0cf82003c30450b619c7e8c23e0adbf21fdb
Pawan Kumar <ratwanapawan@gmail.com>
2024-04-18 13:03:45 +0530

    functionality to navigate each link include hireMe

--- js/index.js
+++ js/index.js
@@ -9,10 +9,14 @@ var typed = new Typed(".typing", {
 
 // Get the aside element and navigation links
 const aside = document.getElementById('aside');
-let navElement = document.querySelectorAll('.nav a');
+// let navElement = document.querySelectorAll('.nav a');
 const mainContainer = document.getElementById('main-container');
-const navList = document.querySelectorAll('.nav li');
-const totalNavList = navList.length;
+const nav = document.querySelector('.nav'),
+    navList = nav.querySelectorAll('li'),
+    totalNavList = navList.length,
+    allSection = document.querySelectorAll(".section"),
+    totalSection = allSection.length;
+
 const menuToggle = document.getElementById('menu-toggle');
 const toggleButton = document.querySelector('.toggle-button');
 
@@ -43,14 +47,63 @@ window.addEventListener('scroll', () => {
 /*==================== Navigate to nav link ==================== */
 
 // Add click event listener to each navigation link
-for (let i = 0; i <= totalNavList - 1; i++) {
+for (let i = 0; i < totalNavList; i++) {
     const a = navList[i].querySelector('a');
     a.addEventListener('click', function () {
+        removeBackSection();
         // Remove active class from all navigation links
         for (let j = 0; j < totalNavList; j++) {
-            navList[j].querySelector(".nav a").classList.remove("active");
+            if (navList[j].querySelector("a").classList.contains("active")) {
+                addBackSection(j);
+            }
+            navList[j].querySelector("a").classList.remove("active")
         }
         // Add active class to the clicked link
         this.classList.add("active");
+        showSection(this)
     });
 }
+
+// Function to remove 'back-section' class from all sections
+function removeBackSection() {
+    for (let i = 0; i < totalSection; i++) {
+        allSection[i].classList.remove('back-section');
+    }
+}
+
+// Function to add 'back-section' class to a specific section
+function addBackSection(num) {
+    allSection[num].classList.add('back-section')
+}
+
+// Function to show a section
+function showSection(element) {
+    for (let i = 0; i < totalSection; i++) {
+        allSection[i].classList.remove('active');
+    }
+    const href = element.getAttribute("href").split('#');
+    const taggetHref = href[1];
+    document.querySelector("#" + taggetHref).classList.add('active')
+}
+
+// Function to update navigation based on the shown section
+function updateNav(element) {
+    const target = element.getAttribute("href").split('#')[1];
+    for (let i = 0; i < totalNavList; i++) {
+        navList[i].querySelector('a').classList.remove('active');
+
+        if (target === navList[i].querySelector('a').getAttribute("href").split("#")[1]) {
+            navList[i].querySelector('a').classList.add('active');
+        }
+    }
+}
+
+// Event listener for 'hire-me' button
+document.querySelector(".hire-me").addEventListener('click', function () {
+    const sectionIndex = this.getAttribute("data-section-index");
+
+    showSection(this);
+    updateNav(this);
+    addBackSection(sectionIndex);
+    removeBackSection();
+})
