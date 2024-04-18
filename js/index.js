b3f173105afcdffcb29fa14ffc95618a60228c2c
Pawan Kumar <ratwanapawan@gmail.com>
2024-04-18 10:16:27 +0530

    add home page

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/*==================== typing Animation ==================== */
+
+var typed = new Typed(".typing", {
+    strings: ["", "Web Designer", "Web Developer", "Graphic Designer", "UI Designer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+});
+
+console.log("Jai Shree Ram")
