f7302ccd085864e77e76592ec02a58e3a613c748
Pawan Kumar <ratwanapawan@gmail.com>
2024-04-18 11:48:11 +0530

    adding functionality for navigate each nav link

--- js/index.js
+++ js/index.js
@@ -9,6 +9,10 @@ var typed = new Typed(".typing", {
 
 // Get the aside element and navigation links
 const aside = document.getElementById('aside');
+let navElement = document.querySelectorAll('.nav a');
+const mainContainer = document.getElementById('main-container');
+const navList = document.querySelectorAll('.nav li');
+const totalNavList = navList.length;
 const menuToggle = document.getElementById('menu-toggle');
 const toggleButton = document.querySelector('.toggle-button');
 
@@ -18,3 +22,16 @@ menuToggle.addEventListener('click', () => {
     aside.classList.toggle('active');
     toggleButton.classList.toggle('tagActive');
 });
+
+// Add click event listener to each navigation link
+for (let i = 0; i <= totalNavList - 1; i++) {
+    const a = navList[i].querySelector('a');
+    a.addEventListener('click', function () {
+        // Remove active class from all navigation links
+        for (let j = 0; j < totalNavList; j++) {
+            navList[j].querySelector(".nav a").classList.remove("active");
+        }
+        // Add active class to the clicked link
+        this.classList.add("active");
+    });
+}
